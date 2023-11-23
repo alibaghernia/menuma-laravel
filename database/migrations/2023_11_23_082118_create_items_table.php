0fba6b44750d4e6c837e1e459b9b7fca6443fb81
alibaghernia <alibaghernia0@gmail.com>
2023-11-23 14:16:37 +0330

    add Item model, migration, policy

---
+++ database/migrations/2023_11_23_082118_create_items_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->string('image_path')->nullable();
+            $table->json('tags')->nullable();
+            $table->json('prices')->nullable();
+            $table->foreignId('category_id')->nullable();
+            $table->foreignId('cafe_restaurant_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('items');
+    }
+};
