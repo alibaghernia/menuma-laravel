128cbb46ac771203b8904a166e1f44dbc2551b3e
alibaghernia <alibaghernia0@gmail.com>
2023-11-23 16:26:48 +0330

    implements FilamentUser in user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,12 +3,15 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
+    implements FilamentUser
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -29,5 +32,11 @@ class User extends Authenticatable
     public function getFullNameAttribute()
     {
         return $this->name . ' ' . $this->family;
+    }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+//        todo
+        return true;
     }
 }
