62b7a9effcd2f64706409380151840a8c309325e
alibaghernia <alibaghernia0@gmail.com>
2023-11-23 16:24:39 +0330

    add family, mobile_number fields in users migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,8 +13,10 @@ return new class extends Migration {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('family')->nullable();
 //            $table->string('email')->unique();
 //            $table->timestamp('email_verified_at')->nullable();
+            $table->string('mobile_number')->unique();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
