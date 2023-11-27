f6e184c6a908717cd364f2c84abaf39ba664180e
alibaghernia <alibaghernia0@gmail.com>
2023-11-27 19:01:16 +0330

    remove use less imports

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,9 +5,6 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 use App\Models\CafeRestaurant;
-use App\Models\Category;
-use App\Models\Item;
-use App\Models\Role;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
