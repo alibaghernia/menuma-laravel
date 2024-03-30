1aa18976ca1251386ff5a845b535c92af11a0ee3
alibaghernia <alibaghernia0@gmail.com>
2024-03-30 22:48:31 +0330

    add theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,4 +7,24 @@ export default {
         './resources/views/**/*.blade.php',
         './vendor/filament/**/*.blade.php',
     ],
+    theme: {
+        extend: {
+            backgroundImage: {
+                'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+                'gradient-conic':
+                    'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+            },
+            colors: {
+                primary: '#3177FF',
+                typography: '#434343',
+                secondary: '#EEB33F',
+                background: '#FBFBFB',
+                more: '#EEB33F',
+            },
+        },
+        fontFamily: {
+            sans: ['Vazirmatn FD','sans-serif'],
+            // vazirmatn: ["Vazirmatn"],
+        },
+    },
 }
