3ee122f2e12e28464453057fe4435848c1cd04aa
alibaghernia <alibaghernia0@gmail.com>
2024-04-13 23:31:20 +0330

    add Footer view component

---
+++ resources/views/components/business/footer.blade.php
@@ -0,0 +1,13 @@
+<div class="mx-10 border-t p-5 border-[#e5e7eb]">
+    @if($show)
+        <div class="flex flex-row place-items-center items-center justify-center gap-2"
+             style="justify-content:center" gap="2">
+            <div class="text-typography/[.8]">میزبانی شده توسط</div>
+            <div class="">
+                <a target="_blank" class="font-bold text-typography" href="https://mtserver.ir">
+                    MT Server
+                </a>
+            </div>
+        </div>
+    @endif
+</div>
