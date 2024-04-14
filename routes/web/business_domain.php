e142a626aa1062a017029d8fa78e5a45692d0532
alibaghernia <alibaghernia0@gmail.com>
2024-04-14 05:51:28 +0330

    add business domain's routes

--- routes/web/business_domain.php
+++ routes/web/business_domain.php
@@ -1,1 +1,32 @@
 <?php
+
+use App\Http\Controllers\Web\BusinessDomain;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/',
+    [BusinessDomain\BusinessController::class, 'profile'])
+    ->name('profile');
+
+Route::get('/menu',
+    [BusinessDomain\BusinessController::class, 'menu'])
+    ->name('menu');
+
+//Route::get('/menu/{categoryId}/{itemId}',
+//    [MainDomain\Tst::class, 'showItem'])
+//    ->name('menu.item');
+
+Route::get('/menu/{categoryId}/{itemId}',
+    [BusinessDomain\BusinessController::class, 'showItem'])
+    ->name('menu.item');
+
+Route::get('/discounts',
+    [BusinessDomain\BusinessController::class, 'conditionalDiscountsList'])
+    ->name('conditional-discounts');
+
+Route::get('/events',
+    [BusinessDomain\BusinessController::class, 'eventsList'])
+    ->name('events');
+
+Route::get('/customer_club/register',
+    [BusinessDomain\BusinessController::class, 'registerInCustomerClub'])
+    ->name('customer-club.register');
