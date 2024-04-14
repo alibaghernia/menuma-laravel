b4f2c3ab7f8510cc4ed2701ca1ebde9fe55a2fdd
alibaghernia <alibaghernia0@gmail.com>
2024-04-14 05:51:46 +0330

    add main domain's routes

--- routes/web/main_domain.php
+++ routes/web/main_domain.php
@@ -1,15 +1,40 @@
 <?php
 
+use App\Http\Controllers\Web\MainDomain;
 use App\Http\Middleware\IsSuperadmin;
 use App\Livewire\QrCodePage;
 use Filament\Http\Middleware\Authenticate;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', [\App\Http\Controllers\Web\MainDomain\Tst::class, 'index']);
-Route::get('/{slug}', [\App\Http\Controllers\Web\MainDomain\Tst::class, 'profile'])
-    ->name('business.profile');
-Route::get('/{slug}/menu', [\App\Http\Controllers\Web\MainDomain\Tst::class, 'menu'])
-    ->name('business.menu');
-    Route::get('/{slug}/customer_club/register', [\App\Http\Controllers\Web\MainDomain\Tst::class, 'registerInCustomerClub'])
-    ->name('business.menu');
+Route::get('/',
+    [MainDomain\Tst::class, 'index'])
+    ->name('home');
+
+Route::name('business.')
+    ->prefix('/{slug}')
+    ->group(function () {
+        Route::get('/',
+            [MainDomain\Tst::class, 'profile'])
+            ->name('profile');
+
+        Route::get('/menu',
+            [MainDomain\Tst::class, 'menu'])
+            ->name('menu');
+
+        Route::get('/menu/{categoryId}/{itemId}',
+            [MainDomain\Tst::class, 'showItem'])
+            ->name('menu.item');
+
+        Route::get('/customer_club/register',
+            [MainDomain\Tst::class, 'registerInCustomerClub'])
+            ->name('customer-club.register');
+
+        Route::get('/discounts',
+            [MainDomain\Tst::class, 'conditionalDiscountsList'])
+            ->name('conditional-discounts');
+//todo
+        Route::get('/events',
+            [MainDomain\Tst::class, 'eventsList'])
+            ->name('events');
+    });
