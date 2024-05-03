b79d4212074a392233dafc787d3f1e1d1d797eee
alibaghernia <alibaghernia0@gmail.com>
2024-05-04 02:28:31 +0330

    Feat: implement menu-bar component

--- app/View/Components/Business/MenuBar.php
+++ app/View/Components/Business/MenuBar.php
@@ -2,18 +2,30 @@
 
 namespace App\View\Components\Business;
 
+use App\Models\CafeRestaurant;
+use App\Models\ConditionalDiscount;
+use App\Models\Event;
 use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
 class MenuBar extends Component
 {
+    public int $countOfConditionalDiscounts;
+    public int $countOfevents;
+
     /**
      * Create a new component instance.
      */
-    public function __construct()
+    public function __construct(
+        public CafeRestaurant $business,
+    )
     {
-        //
+        $this->countOfConditionalDiscounts = ConditionalDiscount::where('cafe_restaurant_id', $business->id)
+            ->count();
+
+        $this->countOfevents = Event::where('cafe_restaurant_id', $business->id)
+            ->count();
     }
 
     /**
@@ -21,6 +33,9 @@ class MenuBar extends Component
      */
     public function render(): View|Closure|string
     {
-        return view('components.business.menu-bar');
+        return view('components.business.menu-bar', [
+            'business' => $this->business,
+            'countOfConditionalDiscounts' => $this->countOfConditionalDiscounts,
+        ]);
     }
 }
