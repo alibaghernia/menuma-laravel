516e796ad1625668b143fc3cf1e875d8de5a3439
alibaghernia <alibaghernia0@gmail.com>
2024-05-04 00:27:00 +0330

    Fix: setLocale does not  work on main domain

--- app/Http/Middleware/SetLocale.php
+++ app/Http/Middleware/SetLocale.php
@@ -15,7 +15,10 @@ class SetLocale
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if (session()->get('lang') === 'en') {
+        if (
+            request()->host() !== config('app.domains.main') &&
+            session()->get('lang') === 'en'
+        ) {
             app()->setLocale('en');
         }
         return $next($request);
