9977e4eece1637f3d14ae366800ef5eae89df800
alibaghernia <alibaghernia0@gmail.com>
2024-05-04 00:49:32 +0330

    Feat: Add translate keys

--- lang/en/pages/menu.php
+++ lang/en/pages/menu.php
@@ -1,6 +1,12 @@
 <?php
 return [
     'offers_of_the_day' => 'Offers of the day',
+    'new' => 'New',
+    'sold_out' => 'Sold out',
     'show_item' => 'Show',
     'toman' => 'T',
+    'single_page' => [
+        'prices' => 'Prices',
+        'price' => 'Price',
+    ],
 ];
