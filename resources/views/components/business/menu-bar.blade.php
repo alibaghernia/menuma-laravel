3e98ba53fe14ba242291719a9f778beb708659f7
alibaghernia <alibaghernia0@gmail.com>
2024-05-19 09:12:16 +0330

    Feat: show customer club in menu just when it is enabled

--- resources/views/components/business/menu-bar.blade.php
+++ resources/views/components/business/menu-bar.blade.php
@@ -148,27 +148,27 @@
                                 </div>
                                 <div class="text-[1rem]">گالری</div>
                             </div>--}}
-                            <a
-                                {{--                                href="/{{$business->slug}}/customer_club/register"--}}
-                                href="{{domain_route('customer-club.register',['slug'=>$business->slug])}}"
-
-                                class="flex flex-row place-items-center items-center justify-normal gap-2 px-[1.5rem] py-[.8rem] border-black transition-[border] duration-[.1s] hover:border-s-[5px] border-b border-b-black/[.1] last:border-b-0 cursor-pointer"
-                                style="justify-content:normal" gap="2">
-                                <div class="">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                         viewBox="0 0 24 24">
-                                        <g fill="#434343" fill-rule="evenodd" clip-rule="evenodd">
-                                            <path
-                                                d="M12 6.25a3.25 3.25 0 1 0 0 6.5a3.25 3.25 0 0 0 0-6.5M10.25 9.5a1.75 1.75 0 1 1 3.5 0a1.75 1.75 0 0 1-3.5 0"></path>
-                                            <path
-                                                d="M12 2.25a7.25 7.25 0 0 0-6.063 11.226l-2.587 4.48a.75.75 0 0 0 .795 1.11l2.614-.514l.861 2.52a.75.75 0 0 0 1.36.133l2.58-4.468a7.313 7.313 0 0 0 .88 0l2.58 4.468a.75.75 0 0 0 1.36-.134l.858-2.526l2.616.52a.75.75 0 0 0 .796-1.11l-2.586-4.479A7.25 7.25 0 0 0 12 2.25M6.25 9.5a5.75 5.75 0 1 1 11.5 0a5.75 5.75 0 0 1-11.5 0m3.734 6.966a7.243 7.243 0 0 1-3.027-1.757l-1.482 2.567l1.637-.322a.75.75 0 0 1 .854.493l.54 1.579zm5.508 2.556l-1.476-2.556a7.244 7.244 0 0 0 3.027-1.757l1.48 2.563l-1.638-.326a.75.75 0 0 0-.856.495z"></path>
-                                        </g>
-                                    </svg>
-                                </div>
-                                <h2 class="text-[1rem]">
-                                    {{__('components/business-header.customer_club')}}
-                                </h2>
-                            </a>
+                            @if($business->has_customer_club)
+                                <a
+                                    href="{{domain_route('customer-club.register',['slug'=>$business->slug])}}"
+                                    class="flex flex-row place-items-center items-center justify-normal gap-2 px-[1.5rem] py-[.8rem] border-black transition-[border] duration-[.1s] hover:border-s-[5px] border-b border-b-black/[.1] last:border-b-0 cursor-pointer"
+                                    style="justify-content:normal" gap="2">
+                                    <div class="">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                             viewBox="0 0 24 24">
+                                            <g fill="#434343" fill-rule="evenodd" clip-rule="evenodd">
+                                                <path
+                                                    d="M12 6.25a3.25 3.25 0 1 0 0 6.5a3.25 3.25 0 0 0 0-6.5M10.25 9.5a1.75 1.75 0 1 1 3.5 0a1.75 1.75 0 0 1-3.5 0"></path>
+                                                <path
+                                                    d="M12 2.25a7.25 7.25 0 0 0-6.063 11.226l-2.587 4.48a.75.75 0 0 0 .795 1.11l2.614-.514l.861 2.52a.75.75 0 0 0 1.36.133l2.58-4.468a7.313 7.313 0 0 0 .88 0l2.58 4.468a.75.75 0 0 0 1.36-.134l.858-2.526l2.616.52a.75.75 0 0 0 .796-1.11l-2.586-4.479A7.25 7.25 0 0 0 12 2.25M6.25 9.5a5.75 5.75 0 1 1 11.5 0a5.75 5.75 0 0 1-11.5 0m3.734 6.966a7.243 7.243 0 0 1-3.027-1.757l-1.482 2.567l1.637-.322a.75.75 0 0 1 .854.493l.54 1.579zm5.508 2.556l-1.476-2.556a7.244 7.244 0 0 0 3.027-1.757l1.48 2.563l-1.638-.326a.75.75 0 0 0-.856.495z"></path>
+                                            </g>
+                                        </svg>
+                                    </div>
+                                    <h2 class="text-[1rem]">
+                                        {{__('components/business-header.customer_club')}}
+                                    </h2>
+                                </a>
+                            @endif
                         </div>
                     </div>
                 </div>
